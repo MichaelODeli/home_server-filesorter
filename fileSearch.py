507fe9395c113e5ceb8e9080901122b333b56af3
Daniel Plotnikov <daniilrew@gmail.com>
2022-02-23 17:18:57 +0300

    search improvements

--- fileSearch.py
+++ fileSearch.py
@@ -1,11 +1,12 @@
 import configparser
+import gc
 
 import fileManager as fman
 
 def searchById(id):
     if fman.isVideoIDExist(id)==True:
         cfg = configparser.ConfigParser()
-        with open('storageLib.ini', 'r', encoding='utf-8') as fp:
+        with open('settings.ini', 'r', encoding='utf-8') as fp:
             cfg.read_file(fp)
         wayConf = cfg.get('settings', 'way')
         if id[0]=='y' or id[0]=='f' or id[0]=='s':
@@ -24,13 +25,65 @@ def searchById(id):
             with open(libName, 'r', encoding='utf-8') as fp:
                 cfg.read_file(fp)
             fullway = wayConf + cfg.get(categ, id)
-            # return [fullway, cfg.get(categ, id).split('/')[0], cfg.get(categ, id).split('/')[1]]
-            # return [categ, id, cfg.get(categ, id).split('/')[0], cfg.get(categ, id).split('/')[1]]
             return [
                 [categ, id, cfg.get(categ, id).split('/')[0], cfg.get(categ, id).split('/')[1]]
             ]
     else:
         return [['Not found']]
+
+def confReaderOptions(name, type):
+    if type == 'keywords':
+        cats = 'keywords'
+    if type == 'filename':
+        if name == 'storageLib_Films.ini':
+            cats = 'films'
+        if name == 'storageLib_Yt.ini':
+            cats = 'youtube'
+        if name == 'storageLib_Serials.ini':
+            cats = 'serials'
+    cfg = configparser.ConfigParser()
+    with open(name, 'r', encoding='utf-8') as fp:
+        cfg.read_file(fp)
+    return(cfg.items(cats, raw=True))
+
+def search(filename, type):
+    cfg = configparser.ConfigParser()
+    with open('settings.ini', 'r', encoding='utf-8') as fp:
+        cfg.read_file(fp)
+    filename = filename.lower().replace(' ', '-')
+    libNames = [cfg.get('libs', 'youtubelib'), cfg.get('libs', 'serialslib'), cfg.get('libs', 'filmslib')]
+    founded = []
+    for n in libNames:
+        g = confReaderOptions(n, type)
+        print(g)
+        for f in g:
+            if filename.lower() in f[1].lower():
+                foundName = f[1]
+                id = f[0]
+                founded.append([id, foundName])
+            if filename.lower() in f[0].lower():
+                foundName = f[1]
+                id = f[0]
+                founded.append([id, foundName])
+    fidex = []
+    if founded!=[]:
+        for naming in founded:
+            id = naming[0]
+            if id[0]=='y':
+                categ='youtube'
+            if id[0]=='f':
+                categ='films'
+            if id[0]=='s':
+                categ='serials'
+            channel = naming[1].split('/')[0]
+            filename = naming[1].split('/')[1]
+            fidex.append([categ, id, channel, filename])
+        return(fidex)
+    else: return[['Not found']]
+
+print(search(filename='корпорация'))
+
+
 """
 # obsolete
 def confReader(name):
@@ -58,7 +111,7 @@ def getContent(foundCategory, foundChannel):
 
 def searchByChannel(channel):
     cfg = configparser.ConfigParser()
-    with open('storageLib.ini', 'r', encoding='utf-8') as fp:
+    with open('settings.ini', 'r', encoding='utf-8') as fp:
         cfg.read_file(fp)
     channelname = channel.lower().replace(' ', '-')
     libNames = [cfg.get('libs', 'youtubelib'), cfg.get('libs', 'serialslib'), cfg.get('libs', 'filmslib')]
@@ -83,51 +136,3 @@ def searchByChannel(channel):
     except UnboundLocalError:
         return [['Not found']]
 """
-
-def confReaderOptions(name):
-    cfg = configparser.ConfigParser()
-    if name == 'storageLib_Films.ini':
-        cats = 'films'
-    if name == 'storageLib_Yt.ini':
-        cats = 'youtube'
-    if name == 'storageLib_Serials.ini':
-        cats = 'serials'
-    with open(name, 'r', encoding='utf-8') as fp:
-        cfg.read_file(fp)
-    return(cfg.items(cats, raw=True))
-
-def search(filename):
-    cfg = configparser.ConfigParser()
-    with open('storageLib.ini', 'r', encoding='utf-8') as fp:
-        cfg.read_file(fp)
-    filename = filename.lower().replace(' ', '-')
-    libNames = [cfg.get('libs', 'youtubelib'), cfg.get('libs', 'serialslib'), cfg.get('libs', 'filmslib')]
-    founded = []
-    for n in libNames:
-        g = confReaderOptions(n)
-        for f in g:
-            if filename.lower() in f[1].lower():
-                foundName = f[1]
-                id = f[0]
-                founded.append([id, foundName])
-            if filename.lower() in f[0].lower():
-                foundName = f[1]
-                id = f[0]
-                founded.append([id, foundName])
-    fidex = []
-    if founded!=[]:
-        for naming in founded:
-            id = naming[0]
-            if id[0]=='y':
-                categ='youtube'
-            if id[0]=='f':
-                categ='films'
-            if id[0]=='s':
-                categ='serials'
-            channel = naming[1].split('/')[0]
-            filename = naming[1].split('/')[1]
-            fidex.append([categ, id, channel, filename])
-        return(fidex)
-    else: return[['Not found']]
-
-# print(searchByFilename(filename='корпорация'))
