04c2644ce905832686859b2307afe87fea5131fa
Daniel Plotnikov <daniilrew@gmail.com>
2022-03-14 20:23:28 +0300

    Add files via upload

--- fileManager.py
+++ fileManager.py
@@ -2,16 +2,24 @@
 import configparser
 import glob
 import os
+g = os.path.dirname(os.path.abspath(__file__)).replace('\\', '/')+'/'
+import HTML
+import datetime
+import sys
+cfg = configparser.ConfigParser()
+with open(g+'settings.ini', 'r', encoding='utf-8') as fp:
+    cfg.read_file(fp)
 
 def optlist(libname, type):
-    cfg = configparser.ConfigParser()
-    with open(libname, 'r', encoding='utf-8') as fp:
-        cfg.read_file(fp)
+    g = os.path.dirname(os.path.abspath(__file__)).replace('\\', '/')+'/'
+    cfgll = configparser.ConfigParser()
+    with open(g+libname, 'r', encoding='utf-8') as fp:
+        cfgll.read_file(fp)
     lst = []
-    i = cfg.options(type)
+    i = cfgll.options(type)
     for opt in i:
-        g = cfg.get(type, opt)
-        p = cfg.get('keywords', opt)
+        g = cfgll.get(type, opt)
+        p = cfgll.get('keywords', opt)
         lstq = [opt, g, p]
         lst.append(lstq)
     return lst
@@ -27,119 +35,208 @@ def checkDoubles(oplistin, filenam):
     if k==0: return True
     else: return False
 
-def listRebuild(type):
-    cfg = configparser.ConfigParser()
-    with open('settings.ini', 'r', encoding='utf-8') as fp:
-        cfg.read_file(fp)
-    if cfg.has_option('libs', type):
+def catsmeow():
+    # cfg = configparser.ConfigParser()
+    # with open('settings.ini', 'r', encoding='utf-8') as fp:
+    #     cfg.read_file(fp)
+    catlist = cfg.options('libs')
+    return catlist
+
+def listRebuild(mode):
+    g = os.path.dirname(os.path.abspath(__file__)).replace('\\', '/')+'/'
+    catlist = catsmeow()
+    # cfg = configparser.ConfigParser()
+    new = []
+    for type in catlist:
+        # with open('settings.ini', 'r', encoding='utf-8') as fp:
+        #     cfg.read_file(fp)
         libPrefix = type[0]
         folderConf = type + 'folder'
         confDir = type + '/'
         libName = cfg.get('libs', type)
-    else:
-        return('type '+type+' is not supported')
-    wayConf = cfg.get('settings', 'way')
-    folderDir = cfg.get('settings', folderConf)
-    # way = wayConf + folderDir + '/*/*.mp4'
-    way = wayConf + folderDir + '/*/*'+cfg.get('extensions', type)
-    ytList = glob.glob(way, recursive=True)
-    ytListFull = []
-    ytListCleaned = []
-    fileWaySplit = []
-    fileWaySplitCleaned = []
-    for fileWay in ytList:
-        fileWay = fileWay.replace('\\', '/')
-        ytListFull.append(fileWay)
-    filesListFull = ytListFull
-    for fileWay in ytList:
-        fileWay = fileWay.replace('\\', '/')
-        fileWay = fileWay.split('/')
-        fileWaySplit.append(fileWay)
-    filesListSplited = fileWaySplit
-    i = 0
-    for flway in filesListFull:
-        fileWay = None
-        filname = filesListSplited[i][-1]
-        fileWay = flway.replace(filname, '')
-        if filname.find(' ')!=(-1):
-            filname = filname.replace(' ', '-')
-            os.rename(flway, fileWay+filname)
-        i+=1
-    ytList = glob.glob(way, recursive=True)
-    for fileWay in ytList:
-        fileWay = fileWay.replace('\\', '/')
-        fileWay = fileWay.replace(wayConf, '')
-        ytListCleaned.append(fileWay)
-    for fileWay in ytList:
-        fileWay = fileWay.replace('\\', '/')
-        fileWay = fileWay.replace(wayConf, '')
-        fileWay = fileWay.split('/')
-        fileWaySplitCleaned.append(fileWay)
-    filesListFullc = ytListCleaned
-    filesListSplitedc = fileWaySplitCleaned
-    cfg = configparser.ConfigParser()
-    with open(libName, 'r', encoding='utf-8') as fp:
-        cfg.read_file(fp)
-    way = wayConf
-    k = 0
-    optlisting = optlist(libName, type) # из файла с параметрами
-    for fileway in filesListSplitedc:
-        file = filesListFullc[k]
-        file = file.replace(confDir, '')
-        category = fileway[0]
-        channel = fileway[1]
-        videoname = fileway[2]
-        id = cfg.options('keywords')
-        id = id[-1]
-        id = id.replace(libPrefix, '')
-        id = str(1 + int(id))
-        id = libPrefix+id
-        if cfg.has_section(channel) == True:
-            # filey = file.replace('.mp4', '')
-            filey = file
-            if checkDoubles(optlisting, filey)==True:
+        wayConf = cfg.get('settings', 'way')
+        folderDir = cfg.get('settings', folderConf)
+        # way = wayConf + folderDir + '/*/*'+cfg.get('extensions', type)
+        way = wayConf + folderDir + '/*/*.*'
+        ytList = glob.glob(way, recursive=True)
+        ytListFull = []
+        ytListCleaned = []
+        fileWaySplit = []
+        fileWaySplitCleaned = []
+        for fileWay in ytList:
+            fileWay = fileWay.replace('\\', '/')
+            ytListFull.append(fileWay)
+        filesListFull = ytListFull
+        for fileWay in ytList:
+            fileWay = fileWay.replace('\\', '/')
+            fileWay = fileWay.split('/')
+            fileWaySplit.append(fileWay)
+        filesListSplited = fileWaySplit
+        i = 0
+        for flway in filesListFull:
+            fileWay = None
+            filname = filesListSplited[i][-1]
+            fileWay = flway.replace(filname, '')
+            if filname.find(' ')!=(-1) or filname.find('%')!=(-1):
+                filname = filname.replace(' ', '-')
+                filname = filname.replace('%', '')
+                # print(flway+' '+fileWay+filname)
+                os.rename(flway, fileWay+filname)
+            i+=1
+        ytList = glob.glob(way, recursive=True)
+        for fileWay in ytList:
+            fileWay = fileWay.replace('\\', '/')
+            fileWay = fileWay.replace(wayConf, '')
+            ytListCleaned.append(fileWay)
+        for fileWay in ytList:
+            fileWay = fileWay.replace('\\', '/')
+            fileWay = fileWay.replace(wayConf, '')
+            fileWay = fileWay.split('/')
+            fileWaySplitCleaned.append(fileWay)
+        filesListFullc = ytListCleaned
+        filesListSplitedc = fileWaySplitCleaned
+        if mode == 'full':
+            f = open(g+libName, 'w')
+            f.close()
+            cfgly = configparser.ConfigParser()
+            with open(g+libName, 'r', encoding='utf-8') as fp:
+                cfgly.read_file(fp)
+            cfgly.remove_section('keywords')
+            cfgly.remove_section(type)
+            cfgly.add_section('keywords')
+            cfgly.set('keywords', '-1', 'None')
+            cfgly.add_section(type)
+            cfgly.set(type, '-1', 'None')
+            with open(g+libName, 'w', encoding='utf-8') as fp:
+                cfgly.write(fp)
+        if mode == 'update':
+            pass
+        cfgl = configparser.ConfigParser()
+        with open(g+libName, 'r', encoding='utf-8') as fp:
+            cfgl.read_file(fp)
+        way = wayConf
+        k = 0
+        optlisting = optlist(libName, type) # из файла с параметрами
+        for fileway in filesListSplitedc:
+            file = filesListFullc[k]
+            file = file.replace(confDir, '')
+            category = fileway[0]
+            channel = fileway[1]
+            videoname = fileway[2]
+            id = cfgl.options('keywords')
+            id = id[-1]
+            id = id.replace(libPrefix, '')
+            id = str(1 + int(id))
+            id = libPrefix+id
+            if cfgl.has_section(channel) == True:
+                # filey = file.replace('.mp4', '')
+                filey = file
+                if checkDoubles(optlisting, filey)==True:
+                    pass
+                else:
+                    cfgl.set(type, id, file)
+                    filen = file.replace('.mp4', '')
+                    cfgl.set('keywords', id, filen)
+                    cfgl.set(channel, id, 'True')
+                    new.append([type, id, file])
+            else:
+                cfgl.add_section(channel)
+                cfgl.set(channel, '-1', 'None')   
+                cfgl.set(type, id, file)
+                filen = file.replace('.mp4', '')
+                cfgl.set('keywords', id, filen)
+                cfgl.set(channel, id, 'True')
+                new.append([type, id, file])
+            k+=1
+        # print('found '+str(k)+' files')
+        for filewayl in optlisting:
+            # print(filewayl)
+            # filel = filewayl[2]
+            filel = filewayl[1]
+            filewayle = way + confDir + filel
+            if os.path.exists(filewayle)==True:
+                # print('found '+filel)
                 pass
             else:
-                cfg.set(type, id, file)
-                filen = file.replace('.mp4', '')
-                cfg.set('keywords', id, filen)
-                cfg.set(channel, id, 'True')
-        else:
-            cfg.add_section(channel)
-            cfg.set(channel, '-1', 'None')   
-            cfg.set(type, id, file)
-            filen = file.replace('.mp4', '')
-            cfg.set('keywords', id, filen)
-            cfg.set(channel, id, 'True')
-        k+=1
-    print('found '+str(k)+' files')
-    for filewayl in optlisting:
-        # print(filewayl)
-        # filel = filewayl[2]
-        filel = filewayl[1]
-        filewayle = way + confDir + filel
-        if os.path.exists(filewayle)==True:
-            # print('found '+filel)
-            pass
-        else:
-            idl = filewayl[0]
-            if idl!=str(-1):
-                channel = (filewayl[2].split('/'))[0]
-                # channel = (filewayl[1].split('/'))[0]
-                cfg.remove_option('keywords', idl)
-                cfg.remove_option(type, idl)
-                cfg.remove_option(channel, idl)
-    with open(libName, 'w', encoding='utf-8') as fp:
-        cfg.write(fp)
+                idl = filewayl[0]
+                if idl!=str(-1):
+                    channel = (filewayl[2].split('/'))[0]
+                    # channel = (filewayl[1].split('/'))[0]
+                    cfgl.remove_option('keywords', idl)
+                    cfgl.remove_option(type, idl)
+                    cfgl.remove_option(channel, idl)
+        with open(g+libName, 'w', encoding='utf-8') as fp:
+            cfgl.write(fp)
+    separater = '<!-- DONOTDELETE -->'
+    wayfy = cfg.get('settings', 'way').replace('storage/', '')
+    # indexHTML = wayfy+'index.html'
+    searchHTML = wayfy+'search.html'
+    updatesHTML = wayfy+'updates.html'
+    # lister = [indexHTML, searchHTML, updatesHTML]
+    lister = [searchHTML, updatesHTML]
+    now = datetime.datetime.now()
+    timing = now.strftime(r'%d.%m.%Y %H:%M:%S')
+    fut = '<p>The file database has been completely updated. Total files in db - {files}</p>\n'
+    putt = '<p>The file database has been partially updated. New {files} files are shown below.</p>\n'
+    alert = '<p>Latest filebase update - {time}</p>'
+    updt = '<h4>{time} - {type} update</h4>\n'
+    if mode == 'full':
+        upd = separater + '\n' + updt.format(time=timing, type='Full') + fut.format(files=str(len(new)))
+    elif mode == 'update' and len(new)>0:
+        upd = separater + '\n' + updt.format(time=timing, type='Partially') + putt.format(files=str(len(new))) + HTML.table(new)
+    else:
+        upd = separater
+    alrt = alert.format(time=timing)
+    for file in lister:
+        with open(file, 'r', encoding='UTF-8') as f:
+            # lines = f.readlines()
+            lines = f.read().splitlines()
+            # lines.split('\n')
+            nf = []
+            for element in lines:
+                if alert.split('-')[0] in element:
+                    nf.append(alrt)
+                else:
+                    nf.append(element)
+            nfn = []
+            for element in nf:
+                if element == r'\n' or element=='' or element==None:
+                    pass
+                else:
+                    nfn.append(element)
+            nf = '\n'.join(nfn)
+        with open(file, 'w', encoding='UTF-8') as f:
+            f.writelines(nf)
+    k = 0
+    with open(updatesHTML, 'r', encoding='UTF-8') as f:
+        lines = f.read().splitlines()
+        nnl = []
+        for element in lines:
+            if element in separater:
+                if k==0:
+                    nnl.append(upd)
+                else: k+=1
+            else:
+                nnl.append(element)
+        nfn = []
+        for element in nnl:
+            if element == r'\n' or element=='' or element==None:
+                pass
+            else:
+                nfn.append(element)
+        nnl = '\n'.join(nfn)
+    with open(updatesHTML, 'w', encoding='UTF-8') as f:
+        f.writelines(nnl)
+            
 
 def isVideoIDExist(id):
-    cfg = configparser.ConfigParser()
-    with open('settings.ini', 'r', encoding='utf-8') as fp:
-        cfg.read_file(fp)
+    # cfg = configparser.ConfigParser()
+    # with open('settings.ini', 'r', encoding='utf-8') as fp:
+    #     cfg.read_file(fp)
     if cfg.has_option('prefixes', id[0])==True:
         categ = cfg.get('prefixes', id[0])
         libName = cfg.get('libs', categ)
-        with open(libName, 'r', encoding='utf-8') as fp:
+        with open(g+libName, 'r', encoding='utf-8') as fp:
             cfg.read_file(fp)
             if cfg.has_option(categ, id)==True:
                 return True
@@ -149,21 +246,34 @@ def isVideoIDExist(id):
         return False
 
 def getLinkId(id):
-    cfg = configparser.ConfigParser()
-    with open('settings.ini', 'r', encoding='utf-8') as fp:
-        cfg.read_file(fp)
+    # cfg = configparser.ConfigParser()
+    # with open('settings.ini', 'r', encoding='utf-8') as fp:
+    #     cfg.read_file(fp)
     # wayConf = cfg.get('settings', 'way')
     wayConf = cfg.get('links', 'webdir')+'storage/'
-    if isVideoIDExist(id)==True:
-        if cfg.has_option('prefixes', id[0]):
-            categ = cfg.get('prefixes', id[0])
-            wayParam = categ + 'folder'
-            wayConf = wayConf + cfg.get('settings', wayParam)+'/'
-            libName = cfg.get('libs', categ)
-            with open(libName, 'r', encoding='utf-8') as fp:
-                cfg.read_file(fp)
+    # if isVideoIDExist(id)==True:
+    if cfg.has_option('prefixes', id[0]):
+        categ = cfg.get('prefixes', id[0])
+        wayParam = categ + 'folder'
+        wayConf = wayConf + cfg.get('settings', wayParam)+'/'
+        libName = cfg.get('libs', categ)
+        with open(g+libName, 'r', encoding='utf-8') as fp:
+            cfg.read_file(fp)
+        if cfg.has_option(categ, id)==True:
             fullway = wayConf + cfg.get(categ, id)
             return fullway
-        else: 
-            return False # check usage!
-    else: return None
+        else:
+            return False
+    else: 
+        return False # check usage!
+
+try:
+    print(sys.argv)
+    if sys.argv[-1]=='full':
+        listRebuild(mode='full')
+    elif sys.argv[-1]=='update':
+        listRebuild(mode='update')
+    else:
+        listRebuild(mode='update')
+except Exception as e:
+    print(e)
--- fileSearch.py
+++ fileSearch.py
@@ -1,44 +1,71 @@
 import configparser
 
-def isVideoIDExist(id):
+def searchById(id):
     cfg = configparser.ConfigParser()
     with open('settings.ini', 'r', encoding='utf-8') as fp:
         cfg.read_file(fp)
-    if cfg.has_option('prefixes', id[0])==True:
+    wayConf = cfg.get('settings', 'way')
+    if cfg.has_option('prefixes', id[0]):
         categ = cfg.get('prefixes', id[0])
+        wayParam = categ + 'folder'
+        wayConf = wayConf + cfg.get('settings', wayParam)
         libName = cfg.get('libs', categ)
         with open(libName, 'r', encoding='utf-8') as fp:
             cfg.read_file(fp)
-            if cfg.has_option(categ, id)==True:
-                return True
-            else:
-                return False
-    else: 
-        return False
-
-def searchById(id):
-    if isVideoIDExist(id)==True:
-        cfg = configparser.ConfigParser()
-        with open('settings.ini', 'r', encoding='utf-8') as fp:
-            cfg.read_file(fp)
-        wayConf = cfg.get('settings', 'way')
-        if cfg.has_option('prefixes', id[0]):
-            categ = cfg.get('prefixes', id[0])
-            wayParam = categ + 'folder'
-            wayConf = wayConf + cfg.get('settings', wayParam)
-            libName = cfg.get('libs', categ)
-            with open(libName, 'r', encoding='utf-8') as fp:
-                cfg.read_file(fp)
+        if cfg.has_option(categ, id)==True:
             return [
                 [categ, id, cfg.get(categ, id).split('/')[0], cfg.get(categ, id).split('/')[1]]
             ]
-    else:
-        return [['Not found']]
+        else:
+            return [['Not found']]
+
+
+# def confReaderOptions(name, type):
+#     if type == 'keywords':
+#         cats = 'keywords'
+#     if type == 'filename':
+#         cats = name.replace('storageLib_', '').replace('.ini', '').lower()
+#     cfg = configparser.ConfigParser()
+#     with open(name, 'r', encoding='utf-8') as fp:
+#         cfg.read_file(fp)
+#     return(cfg.items(cats, raw=True))
+
+# def search(filename, type):
+#     cfg = configparser.ConfigParser()
+#     with open('settings.ini', 'r', encoding='utf-8') as fp:
+#         cfg.read_file(fp)
+#     filename = filename.lower().replace(' ', '-')
+#     libNames = cfg.items('libs')
+#     founded = []
+#     for n in libNames:
+#         g = confReaderOptions(n[1], type)
+#         for f in g:
+#             if filename.lower() in f[1].lower():
+#                 foundName = f[1]
+#                 id = f[0]
+#                 founded.append([id, foundName])
+#             if filename.lower() in f[0].lower():
+#                 foundName = f[1]
+#                 id = f[0]
+#                 founded.append([id, foundName])
+#     fidex = []
+#     if founded!=[]:
+#         for naming in founded:
+#             id = naming[0]
+#             try:
+#                 categ = cfg.get('prefixes', id[0])
+#             except configparser.NoOptionError:
+#                 return[['Not found']]
+#             channel = naming[1].split('/')[0]
+#             filename = naming[1].split('/')[1]
+#             fidex.append([categ, id, channel, filename])
+#         return(fidex)
+#     else: return[['Not found']]
 
 def confReaderOptions(name, type):
     if type == 'keywords':
         cats = 'keywords'
-    if type == 'filename':
+    if type == 'filename' or type == 'channel' or type == 'all':
         cats = name.replace('storageLib_', '').replace('.ini', '').lower()
     cfg = configparser.ConfigParser()
     with open(name, 'r', encoding='utf-8') as fp:
@@ -55,24 +82,42 @@ def search(filename, type):
     for n in libNames:
         g = confReaderOptions(n[1], type)
         for f in g:
-            if filename.lower() in f[1].lower():
-                foundName = f[1]
-                id = f[0]
-                founded.append([id, foundName])
-            if filename.lower() in f[0].lower():
-                foundName = f[1]
-                id = f[0]
-                founded.append([id, foundName])
+            if str(f[0])!='-1':
+                channel_ = f[1].split('/')[0]
+                filename_ = f[1].split('/')[1]
+                if type == 'filename' or type == 'channel' or type == 'keywords':
+                    if type == 'filename':
+                        tofind = filename_
+                    elif type == 'channel':
+                        tofind = channel_
+                    elif type == 'keywords':
+                        tofind = f[1]
+                    else: return[['Incorrect type']]
+                    if filename.lower() in tofind.lower():
+                        foundName = f[1]
+                        id = f[0]
+                        founded.append([id, foundName])
+                elif type == 'all':
+                    if filename.lower() in f[0].lower():
+                        foundName = f[1]
+                        id = f[0]
+                        founded.append([id, foundName])
+                    if filename.lower() in f[1].lower():
+                        foundName = f[1]
+                        id = f[0]
+                        founded.append([id, foundName])
     fidex = []
     if founded!=[]:
         for naming in founded:
             id = naming[0]
-            try:
-                categ = cfg.get('prefixes', id[0])
-            except configparser.NoOptionError:
-                return[['Not found']]
+            # try:
+            categ = cfg.get('prefixes', id[0])
+            # except configparser.NoOptionError:
+            # return[['Not found']]
             channel = naming[1].split('/')[0]
             filename = naming[1].split('/')[1]
             fidex.append([categ, id, channel, filename])
         return(fidex)
     else: return[['Not found']]
+
+# print(search(filename='python', type='keywords'))
