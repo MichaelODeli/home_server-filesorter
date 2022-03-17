e4e494aa4f06fcb457b4bf68f6de297fc825a0b2
Daniel Plotnikov <daniilrew@gmail.com>
2022-03-17 20:57:34 +0300

    Keywords removed

--- fileSearch.py
+++ fileSearch.py
@@ -1,51 +1,78 @@
 import configparser
 
-def isVideoIDExist(id):
+def searchById(id):
     cfg = configparser.ConfigParser()
     with open('settings.ini', 'r', encoding='utf-8') as fp:
         cfg.read_file(fp)
-    if cfg.has_option('prefixes', id[0])==True:
+    wayConf = cfg.get('settings', 'way')
+    if cfg.has_option('prefixes', id[0]):
         categ = cfg.get('prefixes', id[0])
+        wayParam = categ + 'folder'
+        wayConf = wayConf + cfg.get('settings', wayParam)
         libName = cfg.get('libs', categ)
         with open(libName, 'r', encoding='utf-8') as fp:
             cfg.read_file(fp)
-            if cfg.has_option(categ, id)==True:
-                return True
-            else:
-                return False
-    else: 
-        return False
-
-def searchById(id):
-    if isVideoIDExist(id)==True:
-        cfg = configparser.ConfigParser()
-        with open('settings.ini', 'r', encoding='utf-8') as fp:
-            cfg.read_file(fp)
-        wayConf = cfg.get('settings', 'way')
-        if cfg.has_option('prefixes', id[0]):
-            categ = cfg.get('prefixes', id[0])
-            wayParam = categ + 'folder'
-            wayConf = wayConf + cfg.get('settings', wayParam)
-            libName = cfg.get('libs', categ)
-            with open(libName, 'r', encoding='utf-8') as fp:
-                cfg.read_file(fp)
+        if cfg.has_option(categ, id)==True:
             return [
                 [categ, id, cfg.get(categ, id).split('/')[0], cfg.get(categ, id).split('/')[1]]
             ]
-    else:
-        return [['Not found']]
+        else:
+            return [['Not found']]
 
-def confReaderOptions(name, type):
-    if type == 'keywords':
+
+# def confReaderOptions(name, searchType):
+#     if searchType == 'keywords':
+#         cats = 'keywords'
+#     if searchType == 'filename':
+#         cats = name.replace('storageLib_', '').replace('.ini', '').lower()
+#     cfg = configparser.ConfigParser()
+#     with open(name, 'r', encoding='utf-8') as fp:
+#         cfg.read_file(fp)
+#     return(cfg.items(cats, raw=True))
+
+# def search(filename, searchType):
+#     cfg = configparser.ConfigParser()
+#     with open('settings.ini', 'r', encoding='utf-8') as fp:
+#         cfg.read_file(fp)
+#     filename = filename.lower().replace(' ', '-')
+#     libNames = cfg.items('libs')
+#     founded = []
+#     for n in libNames:
+#         g = confReaderOptions(n[1], searchType)
+#         for f in g:
+#             if filename.lower() in f[1].lower():
+#                 foundName = f[1]
+#                 id = f[0]
+#                 founded.append([id, foundName])
+#             if filename.lower() in f[0].lower():
+#                 foundName = f[1]
+#                 id = f[0]
+#                 founded.append([id, foundName])
+#     fidex = []
+#     if founded!=[]:
+#         for naming in founded:
+#             id = naming[0]
+#             try:
+#                 categ = cfg.get('prefixes', id[0])
+#             except configparser.NoOptionError:
+#                 return[['Not found']]
+#             channel = naming[1].split('/')[0]
+#             filename = naming[1].split('/')[1]
+#             fidex.append([categ, id, channel, filename])
+#         return(fidex)
+#     else: return[['Not found']]
+
+def confReaderOptions(name, searchType):
+    if searchType == 'keywords':
         cats = 'keywords'
-    if type == 'filename':
+    if searchType == 'filename' or searchType == 'channel' or searchType == 'all':
         cats = name.replace('storageLib_', '').replace('.ini', '').lower()
     cfg = configparser.ConfigParser()
     with open(name, 'r', encoding='utf-8') as fp:
         cfg.read_file(fp)
     return(cfg.items(cats, raw=True))
 
-def search(filename, type):
+def search(filename, searchType):
     cfg = configparser.ConfigParser()
     with open('settings.ini', 'r', encoding='utf-8') as fp:
         cfg.read_file(fp)
@@ -53,26 +80,41 @@ def search(filename, type):
     libNames = cfg.items('libs')
     founded = []
     for n in libNames:
-        g = confReaderOptions(n[1], type)
+        g = confReaderOptions(n[1], searchType)
         for f in g:
-            if filename.lower() in f[1].lower():
-                foundName = f[1]
-                id = f[0]
-                founded.append([id, foundName])
-            if filename.lower() in f[0].lower():
-                foundName = f[1]
-                id = f[0]
-                founded.append([id, foundName])
+            if str(f[0])!='-1':
+                channel_ = f[1].split('/')[0]
+                filename_ = f[1].split('/')[1]
+                if searchType == 'filename' or searchType == 'channel' or searchType == 'keywords':
+                    if searchType == 'filename':
+                        tofind = filename_
+                    elif searchType == 'channel':
+                        tofind = channel_
+                    elif searchType == 'keywords':
+                        tofind = f[1]
+                    else: return[['Incorrect searchType']]
+                    if filename.lower() in tofind.lower():
+                        foundName = f[1]
+                        founded.append([f[0], foundName])
+                elif searchType == 'all':
+                    if filename.lower() in f[0].lower():
+                        foundName = f[1]
+                        founded.append([f[0], foundName])
+                    if filename.lower() in f[1].lower():
+                        foundName = f[1]
+                        founded.append([f[0], foundName])
     fidex = []
     if founded!=[]:
         for naming in founded:
             id = naming[0]
-            try:
-                categ = cfg.get('prefixes', id[0])
-            except configparser.NoOptionError:
-                return[['Not found']]
+            # try:
+            categ = cfg.get('prefixes', id[0])
+            # except configparser.NoOptionError:
+            # return[['Not found']]
             channel = naming[1].split('/')[0]
             filename = naming[1].split('/')[1]
             fidex.append([categ, id, channel, filename])
         return(fidex)
     else: return[['Not found']]
+
+# print(search(filename='alex', searchType='all'))
